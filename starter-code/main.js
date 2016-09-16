98ae05fcfe5e46211aace36f202804cad5dcf208
Damira <Damira-Mark@marks-MBP.home>
2016-09-16 16:53:48 -0400

    finished logic game

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,4 +1,4 @@
-console.log("JS file is connected to HTML! Woo!");
+/*console.log("JS file is connected to HTML! Woo!");
 
 var cardOne = "king";
 var cardTwo = "queen";
@@ -11,3 +11,36 @@ if(cardTwo === cardFour){
 else {
 	alert("Sorry, try again.");
 }
+*/
+var gameBoard = document.getElementById("game-board");
+var cards = ["king","queen","king","queen"];
+var cardInPlay = [];
+
+var createBoard = function(){
+for(var i = 0; i < cards.length; i++){
+	var newCard = document.createElement('div');
+newCard.className = "card";
+newCard.setAttribute('data-card', cards[i]);
+newCard.addEventListener('click', isTwoCards);
+gameBoard.appendChild(newCard);
+}
+};
+
+function isTwoCards() {
+	cardInPlay.push(this.getAttribute('data-card'));
+	this.innerHTML = '<img src="' + this.getAttribute('data-card')+'.jpg">'
+	if(cardInPlay.length === 2){
+		isMatch(cardInPlay);
+		cardInPlay = [];
+	}
+};
+
+var isMatch = function(array){
+	(array[0] === array[1])?
+	alert("You found a match!") : alert("Sorry, try again.");
+for(var i = 0; i < cards.length; i++){
+	document.getElementsByClassName('card')[i].innerHTML = " ";
+}
+};
+
+createBoard();
